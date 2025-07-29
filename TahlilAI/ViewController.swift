eb106e5380320629e7d9d6b08262888797225d8c
memuhammedyilmaz <yilmaz34muhammed@icloud.com>
2025-07-29 20:58:27 +0300

    Initial Commit

---
+++ TahlilAI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TahlilAI
+//
+//  Created by Muhammed Yılmaz on 29.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
